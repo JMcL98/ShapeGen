dc78b05cf47529f9fb0d4246cff3c85593115be2
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2022-06-08 16:26:13 +0100

    Created ASP.NET API app with example weather functionality

--- ShapeGen/Program.cs
+++ ShapeGen/Program.cs
@@ -1,11 +1,25 @@
-﻿namespace ShapeGen
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static class Program
-    {
-        public static void Main()
-        {
-            Console.WriteLine("Test");
-        }
-    }
- 
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
